53933599be3f2e8d1cc76a0d94b9784fb17db607
yuzuyyy <hendriawan0720@gmail.com>
2025-09-06 14:30:28 +0800

    commit

--- src/pages/About.jsx
+++
@@ -1,8 +0,0 @@
-export default function About() {
-  return (
-    <section className="p-6">
-      <h1 className="text-xl font-bold">This is About Page</h1>
-      <p className="mt-4 text-red-400">Welcome to the about page 🎉</p>
-    </section>
-  );
-}
